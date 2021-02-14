25024ccc9dd8988e477a50821865c9227b7f86d9
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-02-14 15:37:30 -0500

    fixed the background and bottom color

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Background from './Components/Background.jsx';
-import './App.css';
+import Bottom from './Components/Bottom.jsx';
 
 function App() {
   return (
     <div>
        <Background/>
+       <Bottom/>
     </div>
   );
 }
---
+++ task-app/src/Components/Bottom.jsx
@@ -0,0 +1,12 @@
+import '../App.css';
+
+function Bottom() {
+  return (
+    <div>
+       <div className="Bottom">
+      </div>
+    </div>
+  );
+}
+
+export default Bottom;
