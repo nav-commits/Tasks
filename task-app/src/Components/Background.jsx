19fb51ccbf74313d90c1c751f7e708085aff0a8a
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-02-14 14:57:22 -0500

    fixed padding of background to rem for top and bottom

--- task-app/src/Components/Background.jsx
+++ task-app/src/Components/Background.jsx
@@ -4,8 +4,8 @@ function Background() {
   return (
     <div>
        <div className="Back-ground">
-        <h1>Tasks</h1>
-       </div>
+          <h1>Tasks</h1>
+      </div>
     </div>
   );
 }
