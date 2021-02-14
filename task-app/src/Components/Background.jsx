372bf744d4ddac1a2ac8dac61cbbfce8025da6e2
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-02-14 15:30:26 -0500

    added favicon, background image and fixed header

--- task-app/src/Components/Background.jsx
+++ task-app/src/Components/Background.jsx
@@ -4,7 +4,7 @@ function Background() {
   return (
     <div>
        <div className="Back-ground">
-          <h1>Tasks</h1>
+          <h1 className="Tasks-header">Tasks</h1>
       </div>
     </div>
   );
