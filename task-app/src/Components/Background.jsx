0ebd8d8bac4787ec4058caa823c3eeb59cccb261
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-02-14 14:49:40 -0500

    deleted stuff not needed in react starter pack, created my components and styling

---
+++ task-app/src/Components/Background.jsx
@@ -0,0 +1,13 @@
+import '../App.css';
+
+function Background() {
+  return (
+    <div>
+       <div className="Back-ground">
+        <h1>Tasks</h1>
+       </div>
+    </div>
+  );
+}
+
+export default Background;
