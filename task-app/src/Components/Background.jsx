04ad1bea83fce2efed12b40e1b29a327bf47ae58
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-02-14 22:49:50 -0500

    fixed the padding added input

--- task-app/src/Components/Background.jsx
+++ task-app/src/Components/Background.jsx
@@ -5,6 +5,7 @@ function Background() {
     <div>
        <div className="Back-ground">
           <h1 className="Tasks-header">Tasks</h1>
+          <input className="input-task" type="text"/>
       </div>
     </div>
   );
